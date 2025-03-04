c49cc147d6d35c15e1a585b32d738540ee65cf79
Mekasu <mekasurenae@gmail.com>
2025-03-03 22:34:27 -0600

    started basic header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,23 +1,66 @@
 import {
   HeaderContainer,
   ImageContainer,
+  UserContainer,
   TitleContainer,
   AppIcon,
   Title,
-  SubTitle
+  SubTitle,
+  Username
 } from '../styles/components/header.style';
 
+import { useAuth } from '../hooks/authContext';
+import { useState, useEffect } from 'react';
+
+import api from '../hooks/api';
+
 export default function Header() {
+  const [user, setUser] = useState(null);
+
+  const userExistingId = useAuth();
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await api.get("/users/", userExistingId);
+
+        if (response.status === 200) {
+          setUser(response.data.user);
+        };
+      } catch (err) {
+        setUser(null);
+      };
+    };
+
+    getData();
+  }, [userExistingId, setUser]);
+
+  const getUsername = () => {
+    if (user && user.username) {
+      return (
+        <Username>{user.username}</Username>
+      );
+    } else {
+      return (
+        <Username>Hello, User!</Username>
+      );
+    };
+  };
+
   return (
     <HeaderContainer>
       <ImageContainer>
         <AppIcon
           src="/images/icon.png"
           alt="Mek's Speak Icon"
-          width="110"
-          height="110"
+          width="80"
+          height="80"
           />
       </ImageContainer>
+
+      <UserContainer>
+        { getUsername() } 
+      </UserContainer>
 
       <TitleContainer>
         <Title>
