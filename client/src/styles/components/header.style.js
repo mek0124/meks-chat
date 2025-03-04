4065c09f980585781c5a1f757e97dcf554b9b073
Mekasu <mekasurenae@gmail.com>
2025-03-03 22:19:43 -0600

    started header styles

---
+++ client/src/styles/components/header.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div``;
+
+export const ImageContainer = styled(HeaderContainer)``;
+
+export const TitleContainer = styled(HeaderContainer)``;
+
+export const AppIcon = styled.img``;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h3``;
