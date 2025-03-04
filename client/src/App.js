ce03abed90752e2999241069cbebd3ce01ef7610
Mekasu <mekasurenae@gmail.com>
2025-03-03 22:17:59 -0600

    added routes; added auth context; added global styles;

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/authContext';
 
-function App() {
+import GlobalStyle from "./styles/globalStyles";
+
+import Header from './components/header';
+
+import Home from './pages/home';
+import SignUp from './pages/auth/signup';
+import Login from './pages/auth/login';
+import Dashboard from './pages/dashboard/dashboard';
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Header />
+      
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/auth">
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </AuthProvider>
     </div>
   );
-}
-
-export default App;
+};
